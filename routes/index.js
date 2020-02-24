306b4887fd9c04088a6b75404164c1664bea539a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-24 10:23:53 +0100

    controller for users

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const { Router } = require('express');
-const controllers = require('../controllers');
+const controllers = require('../controllers/users');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome'));
 
 router.post('/users', controllers.createUser);
-
 router.get('/users', controllers.getAllUsers);
 router.get('/users/:userId', controllers.getUserById);
 router.put('/users/:userId', controllers.updateUser);
