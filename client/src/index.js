ab01e61af5f33c205200e436f51a118726c4e5da
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-24 19:00:53 +0100

    more errors with react

--- client/src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,33 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // the output bundle won't be optimized for production but suitable for development
-  mode: 'development',
-  // the app entry point is /src/index.js
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-  	// the output of the webpack build will be in /dist directory
-    path: path.resolve(__dirname, 'dist'),
-    // the filename of the JS bundle will be bundle.js
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-      	// for any file with a suffix of js or jsx
-        test: /\.jsx?$/,
-        // ignore transpiling JavaScript from node_modules as it should be that state
-        exclude: /node_modules/,
-        // use the babel-loader for transpiling JavaScript to a suitable format
-        loader: 'babel-loader',
-        options: {
-          // attach the presets to the loader (most projects use .babelrc file instead)
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
         }
-      }
-    ]
-  },
-  // add a custom index.html as the template
-  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
-};
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
