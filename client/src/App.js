a0125c3f2cc97d4c3a16487a718f39d5b5033603
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-26 17:13:34 +0100

    navbar for client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="">
+      <Nav />
+      <h1>Welcome to Fantasy Stock Trade</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <ul className="navbar-nav mr-auto">
+                <li>
+                    <a href="/" className="nav-link">
+                        Fantasy Stock Trade
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
