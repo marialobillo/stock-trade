916a9833ceefd87bfb5d33394938b7dec9a47477
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-29 12:49:19 +0100

    seeds for Investments

--- database/seeders/20200229113844-Investment.js
+++ database/seeders/20200229113844-Investment.js
@@ -2,25 +2,21 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
+  
+      return queryInterface.bulkInsert('Investment', [{
+        userId: 2,
+        company: 'Apple Inc.',
+        symbol: 'APPL',
+        priceBuy: '134.23',
+        priceSell: null,
+        isActive: true,
+        dateBuy: now(),
+        dateSell: null,
       }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+   
+      return queryInterface.bulkDelete('Investmnet', null, {});
   }
 };
