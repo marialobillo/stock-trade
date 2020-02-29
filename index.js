972323308cd5e09421f739063c5b6aeb40fd8e9a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-29 16:19:20 +0100

    fixing database, making it easier

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const server = require('./server');
-
-const PORT = process.env.PORT || 3300;
-
-server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
