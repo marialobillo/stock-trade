128927aa1155e9e36da2ce5a22f9256f3c993a84
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-29 17:34:10 +0100

    adding data using seeds

--- database/seeders/20200229162617-User.js
+++ database/seeders/20200229162617-User.js
@@ -2,25 +2,27 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
+  
+      return queryInterface.bulkInsert('Users', [{
         name: 'John Doe',
-        isBetaMember: false
+        email: 'john@mail.com',
+        password: '123456',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@mail.com',
+        password: '123456',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       }], {});
-    */
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    
+      return queryInterface.bulkDelete('Users', null, {});
+    
   }
 };
--- database/seeders/20200229162630-Holding.js
+++ database/seeders/20200229162630-Holding.js
@@ -2,25 +2,40 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    
+      return queryInterface.bulkInsert('Holdings', [{
+        userId: 1,
+        company: 'Apple Inc.',
+        symbol: 'APPL',
+        shares: 20,
+        priceBuy: 170.56,
+        priceSell: null,
+        isActive: true,
+        dateBuy: new Date(),
+        dateSell: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        company: 'Ebay Inc.',
+        symbol: 'EBAY',
+        shares: 30,
+        priceBuy: 123.44,
+        priceSell: 160.99,
+        isActive: false,
+        dateBuy: new Date(),
+        dateSell: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {});
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    
+      return queryInterface.bulkDelete('Holdings', null, {});
+    
   }
 };
