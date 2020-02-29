52138245f4193dfdaf5c687775cff966ff45c5cd
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-29 11:41:45 +0100

    removing old holding and transactions

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const { Router } = require('express');
 const UserController = require('../controllers/users');
 const SymbolController = require('../controllers/symbols');
-const HoldingController = require('../controllers/holdings');
-const TransactionController = require('../controllers/transaction');
 
 const router = Router();
 
@@ -25,20 +23,5 @@ router.get('/symbols/:symbolId', SymbolController.getSymbolById);
 router.put('/symbols/:symbolId', SymbolController.updateSymbol);
 router.delete('/symbols/:symbolId', SymbolController.deleteSymbol);
 
-// Holding routes
-router.get('/holdings', HoldingController.getAllHoldings);
-router.post('/holdings', HoldingController.createHolding); // Buy a Holding
-router.get('/holdings/:holdingId', HoldingController.getHoldingById);  
-router.put('/holdings/:holdingId', HoldingController.updateHolding);
-router.delete('/holdings/:holdingId', HoldingController.deleteHolding);
-router.get('/holdings/active/:userId', HoldingController.getActiveHoldingsByUserId); // by userId
-
-// Transactions routes
-router.get('/transactions', TransactionController.getAllTransactions);
-router.post('/transactions', TransactionController.createTransaction);
-router.get('/transactions/:transactionId', TransactionController.getTransactionById);
-router.put('/transactions/:transactionId', TransactionController.updateTransaction);
-router.delete('/transactions/:transactionId', TransactionController.deleteTransaction);
-router.get('transactions/byHolding/:holdingId');
 
 module.exports = router;
