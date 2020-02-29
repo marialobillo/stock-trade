42f36aa5b8c11c19c42b76c7102770eb87d4d2be
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-02-29 16:45:12 +0100

    databases

--- database/config/config.js
+++ database/config/config.js
@@ -1,26 +1,16 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
 }
