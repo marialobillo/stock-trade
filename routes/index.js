a51a6a4c1bea64bd3a43091a1f5b908458cea622
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-03-01 12:19:45 +0100

    checking user controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const { Router } = require('express');
+const UserController = require('../controllers/users');
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('Welcome'))
+router.get('/', (req, res) => res.send('Welcome'));
+
+// User routes
+router.post('/users/register', UserController.registerUser); // Register User
+router.get('/users', UserController.getAllUsers);
+router.get('/users/:userId', UserController.getUserById);
+router.put('/users/:userId', UserController.updateUser);
+router.delete('/users/:userId', UserController.deleteUser);
+
+router.post('/login', UserController.loginUser); // Login User
+router.post('/whoami', UserController.whoami); // Checking token exists on http headers
 
 module.exports = router;
