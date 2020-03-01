b366c0a4356f9c33c8a6470eeef2423fe2587316
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-03-01 19:31:32 +0100

    showing errors on dashboard

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -1,43 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import Error from './../components/error';
 
 export default function Dashboard({ user }) {
     const [holdings, setHoldings] = useState([]);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
         async function loadInfo() {
-                
-                const url_holdings = `http://localhost:3300/api/holdings/${user.id}`;
-                try {
-                    const data_holdings = await Axios.get(url_holdings);
-                    const loadedHoldings = data_holdings.data.holdings;
-                    setHoldings(loadedHoldings);
 
-                } catch (error) {
-                    console.log(error.message);
-                }
+            const url_holdings = `http://localhost:3300/api/holdings/${user.id}`;
+            try {
+                const data_holdings = await Axios.get(url_holdings);
+                const loadedHoldings = data_holdings.data.holdings;
+                setHoldings(loadedHoldings);
+
+            } catch (error) {
+                console.log(error.message);
+            }
         }
 
         loadInfo();
     }, []);
 
+    function showError(message) {
+        setError(message);
+    }
 
-    async function handleSubmit(holding){
+    function hideError() {
+        setError(null);
+    }
+
+    async function handleUpdate(holding) {
 
         holding['priceSell'] = 340.55;
         holding['isActive'] = false;
-        holding['dateSell'] = new Date().toISOString().slice(0,10);
-        
+        holding['dateSell'] = new Date().toISOString().slice(0, 10);
+
         try {
             const url = `http://localhost:3300/api/holdings/${holding.id}`;
-            const {data} = Axios.put(url, holding);
+            const { data } = Axios.put(url, holding);
         } catch (error) {
             console.log(error.message)
         }
-    
+
     }
     return (
         <div className="container">
+            <Error message={error} hideError={hideError} />
             <div className="card">
                 <span>Welcome {user.name} to your DashBoard!!!</span>
             </div>
@@ -55,15 +66,15 @@ export default function Dashboard({ user }) {
                         </tr>
                     </thead>
                     <tbody>
-                    {holdings.map(holding => (
-                        <tr key={holding.id}>
-                            <td>{holding.company}</td>
-                            <td>{holding.shares}</td>
-                            <td>{holding.priceBuy}</td>
-                            <td>{holding.dateBuy}</td>
-                            <td>{holding.isActive ? 'YES' : 'SOLD'}</td>
-                            <td><span className="btn btn-danger" onClick={() => handleSubmit(holding)}>Sell</span></td>
-                        </tr>))}
+                        {holdings.map(holding => (
+                            <tr key={holding.id}>
+                                <td>{holding.company}</td>
+                                <td>{holding.shares}</td>
+                                <td>{holding.priceBuy}</td>
+                                <td>{holding.dateBuy}</td>
+                                <td>{holding.isActive ? 'YES' : 'SOLD'}</td>
+                                <td><span className="btn btn-danger" onClick={() => handleUpdate(holding)}>Sell</span></td>
+                            </tr>))}
                     </tbody>
                 </table>
             </div>
