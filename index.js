68809c9387bf2335068cf03f929fb3a5e3aefac7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-01-08 20:25:17 +0100

    create a new holding

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const holdings = [
   {
-    id: '1234123',
+    id: '123123',
     company: 'APPLE Inc.',
     symbol: 'APPL',
     shares: 20,
     priceBuy: 203.33,
     priceSell: 0,
-    isActive: true,
+    isActive: true
   },
   {
-    id: '1234124',
+    id: '123124',
     company: 'EBAY Inc.',
     symbol: 'EBAY',
     shares: 10,
     priceBuy: 140.33,
     priceSell: 0,
-    isActive: true,
+    isActive: true
   },
   {
-    id: '1234125',
+    id: '123125',
     company: 'Facebook Inc.',
     symbol: 'FCBK',
     shares: 40,
     priceBuy: 403.33,
     priceSell: 0,
-    isActive: true,
+    isActive: true
   }
 ]
 
 app.route('/holdings')
   .get((req, res) => {
     res.json(holdings)
+  })
+  .post((req, res) => {
+    let newHolding = req.body
+    console.log('Holding ---> ', newHolding)
+    if(!newHolding.symbol || !newHolding.shares || !newHolding.priceBuy){
+      
+      // Bad request
+      res.status(400).send('Symbol, shares, and priceBuy are requirements.')
+      return 
+    }
+
+    newHolding.id = uuidv4()
+    holdings.push(newHolding)
+    // Created
+    res.status(201).json(newHolding)
   })
 
 app.get('/holdings/:id', (req, res) => {
@@ -44,6 +63,9 @@ app.get('/holdings/:id', (req, res) => {
       return
     }
   }
+
+  // Not found 
+  res.status(404).send(`The holding with id ${res.params.id} does not exist.`)
 })
 
 
