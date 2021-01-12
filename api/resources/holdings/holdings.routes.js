ddee09e065924e925c400b3988e414d70b0e4b4a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-01-12 20:29:00 +0100

    adding auth to holding routes

--- api/resources/holdings/holdings.routes.js
+++ api/resources/holdings/holdings.routes.js
@@ -2,25 +2,27 @@ const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 const _ = require('underscore')
 const logger = require('./../../../utils/logger')
+const passport = require('passport')
 
 const holdingsValidate = require('./holdings.validate')
 
+const jwtAuthenticate = passport.authenticate('jwt', { session: false })
 const holdings = require('./../../../database').holdings
 const holdingsRouter = express.Router()
-
-
 
 
 holdingsRouter.get('/', (req, res) => {
   res.json(holdings)
 })
 
-holdingsRouter.post('/', holdingsValidate, (req, res) => {
-  let newHolding = req.body
+holdingsRouter.post('/', [jwtAuthenticate, holdingsValidate], (req, res) => {
+  let newHolding = {
+    ...req.body, 
+    id: uuidv4(),
+    owner: req.user.username
+  }
   
-  newHolding.id = uuidv4()
   holdings.push(newHolding)
-
   logger.info("Holding created added to the wallet", newHolding)
   res.status(201).json(newHolding)
 })
@@ -38,29 +40,32 @@ holdingsRouter.get('/:id', (req, res) => {
 })
 
 
-holdingsRouter.put('/:id',(req, res) => {
-  let id = req.params.id 
-  let udpatedHolding = req.body 
-
-  if(!udpatedHolding.symbol || !udpatedHolding.shares || !udpatedHolding.priceBuy){
-    // Bad request
-    res.status(400).send('Symbol, shares, and priceBuy are requirements.')
-    return 
+holdingsRouter.put('/:id', [jwtAuthenticate, holdingsValidate], (req, res) => {
+  let holdingUpdated = {
+    ...req.body,
+    id: req.params.id, 
+    owner: req.user.username
   }
 
-  let index = _.findIndex(holdings, holding => holding.id == id)
+
+  let index = _.findIndex(holdings, holding => holdingUpdated.id == id)
 
   if(index !== -1){
-    udpatedHolding.id = id
-    holdings[index] = udpatedHolding
-    res.status(200).json(udpatedHolding)
+    if(holdings[index].owner !== holdingsValidate.owner){
+      logger.info(`User ${req.user.username} is not the owner of ${holdingUpdated.id} holding.`)
+      res.status(401).send(`You are not the holding owner. Only can SEE your own holdings.`)
+      return
+    }
+    holdings[index] = holdingUpdated
+    logger.info(`Product id ${holdingUpdated} was updated.`)
+    res.status(200).json(holdingUpdated)
   } else {
-    res.status(404).send(`The holding with id ${id} does not exist.`)
+    res.status(404).send(`The holding with id ${holdingUpdated.id} does not exist.`)
   }
 })
 
 
-holdingsRouter.get('/:id', (req, res) => {
+holdingsRouter.get('/:id', jwtAuthenticate, (req, res) => {
   let index = _.findIndex(holdings, holding => holding.id == req.params.id)
   
   if(index === -1){
@@ -68,6 +73,14 @@ holdingsRouter.get('/:id', (req, res) => {
     res.status(404).send(`Holding with id ${req.params.id} does not exist.`)
     return 
   }
+
+  if(holdings[index].owner !== req.user.username){
+    logger.info(`User ${req.user.username} is not the owner of ${holdings[index].id} holding.`)
+    res.status(401).send(`You are not the holding owner. Only can DELETE your own holdings.`)
+    return
+  }
+
+  logger.info(`Product id ${req.params.id} was deleted.`)
 
   let deleted = holdings.splice(index, 1)
   res.status(200).json(deleted)
