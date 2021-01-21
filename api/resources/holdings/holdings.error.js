b15e37522bc43b71e93069f0f5861682c6234d95
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-01-21 20:01:49 +0100

    errors custom for holdings

---
+++ api/resources/holdings/holdings.error.js
@@ -0,0 +1,23 @@
+class HoldingNoExists extends Error {
+    constructor(message){
+        super(message);
+        this.message = message || 'Holding Does not Exist.'
+        this.status = 404
+        this.name = 'HoldingNoExists'
+    }
+}
+
+class UserNoOwner extends Error {
+    constructor(message){
+        super(message){
+            this.message = message || 'You are not the holding owner.'
+            this.status = 401
+            this.name = 'UserNotOwner'
+        }
+    }
+}
+
+module.exports = {
+    HoldingNoExists,
+    UserNoOwner
+}
