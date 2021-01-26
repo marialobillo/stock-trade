41f40bb632b30727033c372017677a4b86fdb74a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-01-26 12:35:57 +0100

    updating the client part

--- client/src/components/loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loading() {
-    return (
-        <div className="spinner-grow" role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-    );
-}
