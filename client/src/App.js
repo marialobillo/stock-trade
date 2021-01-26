953810ef0ef3c2b6f55771bc9f88bbc6e07f1a4d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-01-26 13:47:28 +0100

    view for login updating

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Dashboard from './components/dashboard/Dashboard'
+
 
 function App() {
   return (
     <div>
-      <h1>Stock Trade App</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/dashboard" component={Dashboard} />
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,81 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+    const [user, saveUser] = useState({
+        username: '',
+        password: ''
+    })
+
+    const { username, password } = user
+
+    const onChange = event => {
+        saveUser({
+            ...user, 
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // on submit
+    const onSubmit = event => {
+        event.preventDefault()
+
+        // Validate
+    }
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <span className="navbar-brand" href="#">Stock Trade App </span>
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                </div>
+            </nav>
+            <div className="container text-center">
+                <div className="login-panel">
+                    <h3>Welcome</h3>
+                    <br/>
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                placeholder="Username..."
+                                className="form-control"
+                                value={username}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Password..."
+                                className="form-control"
+                                value={password}
+                                onChange={onChange}
+                            />
+                        </div>
+
+                        <div className="from-group">
+                            <input
+                                type="submit"
+                                className="btn btn-block btn-primary"
+                                value="Login"
+                            />
+                        </div>
+                    </form>
+
+                    <br/>
+                    <Link to={'/register'} className="">
+                        Get a New Account
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
