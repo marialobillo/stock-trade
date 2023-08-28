7119407cdb4dfe21070416212952a73f9b301370
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-08-28 19:14:20 +0200

    feat: add new model for user

---
+++ api/src/models/User.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from '../interfaces/user.interface';
+
+const UserSchema: Schema = new Schema<IUser>(
+    {
+        username: { type: String, required: true },
+        email: { type: String, required: true, unique: true }, 
+        password: { type: String, required: true }
+    }, 
+    {
+        timestamps: true, 
+        versionKey: false
+    }
+)
+
+const UserModel = model<IUser>('User', UserSchema)
+
+export default UserModel;
