837dfb88a42f596e542042b7034faba0120e2ab9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-08-28 20:22:02 +0200

    refactor: add extra features to password field on user model

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -5,7 +5,7 @@ const UserSchema: Schema = new Schema<IUser>(
     {
         username: { type: String, required: true },
         email: { type: String, required: true, unique: true }, 
-        password: { type: String, required: true }
+        password: { type: String, required: true, minlength: 6, maxlength: 255 }
     }, 
     {
         timestamps: true, 
