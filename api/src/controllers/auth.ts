d00f85e223566d55b4c6d627d84945ff40c0d469
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-08-28 20:26:53 +0200

    refactor: add register new user route and controller

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -1,2 +1,12 @@
 import { Request, Response } from 'express';
 import { registerNewUser } from '../services/auth';
+
+const registerController = async (req: Request, res: Response) => {
+    const { email, password, username} = req.body;
+    const responseUser = await registerNewUser({ email, password, username });
+    return res.status(200).json(responseUser);
+}
+
+export { 
+    registerController 
+}
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { registerController } from '../controllers/auth';
 
 const router = Router();
 
