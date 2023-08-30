2332e26ceb4119579982333c02abd8e4a127a8e3
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-08-30 17:27:11 +0200

    refactor: change attributes for holdings

--- api/src/models/Holding.ts
+++ api/src/models/Holding.ts
@@ -7,10 +7,10 @@ const HoldingSchema: Schema = new Schema<IHolding>(
         symbol: { type: String, required: true, minlength: 3 },
         shares: { type: Number, default: 0 },
         priceBuy: { type: Number, required: true },
-        priceSell: { type: Number, required: true },
+        priceSell: { type: Number },
         dateBuy: { type: Date, default: Date.now },
-        dateSell: { type: Date, required: true },
-        isActive: { type: Boolean, required: true },
+        dateSell: { type: Date},
+        isActive: { type: Boolean, default: true },
         owner: { type: String, required: true }
     }, 
     {
