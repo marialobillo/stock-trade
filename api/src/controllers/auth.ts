293e921785d66c9dbacf9cc9d21bd50684ed60df
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-08-31 12:07:05 +0200

    fix: refactor: export for handle http error and using it for register and login

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -1,19 +1,28 @@
 import { Request, Response } from 'express';
 import { registerNewUser, loginUser } from '../services/auth';
+import { handleHttp } from '../utils/error.handle';
 
 const registerController = async (req: Request, res: Response) => {
-    const { email, password, username} = req.body;
-    const responseUser = await registerNewUser({ email, password, username });
-    return res.status(200).json(responseUser);
+    try {
+        const { email, password, username} = req.body;
+        const responseUser = await registerNewUser({ email, password, username });
+        return res.status(200).json(responseUser); 
+    } catch (error) {
+        handleHttp(res, 'ERROR_REGISTERING_USER')
+    }
 }
 
 const loginController = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    const responseUser = await loginUser({ email, password })
-    if(responseUser === 'USER_NOT_FOUND' || responseUser === 'INCORRECT_PASSWORD') {
-        return res.status(404).json({ message: 'User not found' })
-    }
-    return res.status(200).json(responseUser);      
+    try {
+        const { email, password } = req.body;
+        const responseUser = await loginUser({ email, password })
+        if(responseUser === 'USER_NOT_FOUND' || responseUser === 'INCORRECT_PASSWORD') {
+            return res.status(404).json({ message: 'User not found' })
+        }
+        return res.status(200).json(responseUser);
+    } catch (error) {
+        handleHttp(res, 'ERROR_LOGING_USER')
+    }     
 }
 
 export { 
--- api/src/utils/error.handle.ts
+++ api/src/utils/error.handle.ts
@@ -4,3 +4,5 @@ const handleHttp = (res: Response, error: string) => {
     res.status(500)
     res.send({ error })
 }
+
+export { handleHttp }
