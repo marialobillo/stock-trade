eff218474f11b34bb0dcdc0ac94bf132dfcd66ef
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-09-20 17:06:57 +0200

    feat: add loading component

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div className="loading">
+            <div className="dot-1"/>
+            <div className="dot-2"/>
+            <div className="dot-3"/>
+        </div>
+    )
+}
+
+export default Loading
