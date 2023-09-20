3b34bfdbec9f3f5b234790741a689b32076347cc
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-09-20 16:09:00 +0200

    feat: add auth helpers, handle auth token

--- client/src/helpers/authHelpers.ts
+++ client/src/helpers/authHelpers.ts
@@ -1,2 +1,36 @@
 import Axios from 'axios';
 
+const TOKEN_KEY = process.env.TOKEN_KEY || 'token';
+
+export const setToken = (token: string) => {
+    localStorage.setItem(TOKEN_KEY, token);
+}
+
+export const getToken = () => {
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+export const deleteToken = () => {
+    localStorage.removeItem(TOKEN_KEY);
+}
+
+export const initAxiosInterceptors = () => {
+    Axios.interceptors.request.use((config) => {
+        const token = getToken();
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    });
+
+    Axios.interceptors.response.use(
+        (response) => response, 
+        (error) => {
+            if (error.response.status === 401) {
+                deleteToken();
+                window.location.replace('/login');
+            } else {
+                return Promise.reject(error);
+            }      
+    })
+}
