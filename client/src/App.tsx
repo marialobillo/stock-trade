c830548591684cd794082674213619365ce77e8c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-09-25 17:04:31 +0200

    feat: add browserrouter on main file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
-
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { isSetup } from './helpers/authHelpers'
 
+
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+
 function App() {
+
+  const [user, setUser] = useState(null)
+  const [loadingUser, setLoadingUser] = useState(true)
+  const [error, setError] = useState(null)
 
 
   const handleClick = () => {
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
