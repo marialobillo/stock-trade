08adec3ddfa1842974e78dab725626f5e8f86d82
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-09-25 19:19:45 +0200

    fix: add register props

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,99 @@
 import Main from "../components/Main";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ChangeEvent, FormEvent } from "react";
 
-const Register = () => {
+
+type RegisterProps = {
+    user: {
+        username: string;
+        email: string;
+        password: string;
+    };
+};
+
+const Register = ({ user }: RegisterProps ) => {
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    }
   return (
     <Main center={true}>
-        <h1>Register</h1>
+        <Container>
+        <div className="form-panel panel-body">
+                    <h2>Register</h2>
+
+                    <form
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                min="3"
+                                max="100"
+                                required
+                                placeholder="Your Username..."
+                                className="form-control"
+                                onChange={onChange}
+                                value={user.username}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                required
+                                placeholder="Your Email..."
+                                className="form-control"
+                                onChange={onChange}
+                                value={user.email}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                required
+                                placeholder="Your Password..."
+                                className="form-control"
+                                onChange={onChange}
+                                value={user.password}
+                            />
+                        </div>
+
+                        <div className="from-group">
+                            <input
+                                type="submit"
+                                className="btn btn-block btn-warning"
+                                value="Register"
+                            />
+                        </div>
+                    </form>
+
+                    <div>
+                        <br/>
+                        Already has an account? 
+                    <Link to="/login" className="text-warning">
+                        Login
+                    </Link>
+
+                    </div>
+
+                </div>
+        </Container>
     </Main>
   )
 }
